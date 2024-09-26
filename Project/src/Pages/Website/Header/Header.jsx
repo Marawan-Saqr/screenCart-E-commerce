b5ca98fcef2556234475edce14f4dea33b218ab9
Marawan Saqr <loew.krs@yahoo.com>
2024-09-26 11:13:07 +0300

    Added Header Component

---
+++ Project/src/Pages/Website/Header/Header.jsx
@@ -0,0 +1,42 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row align-items-center justify-content-between">
+          <div className="col-md-12 col-lg-3">
+            <div className="left">
+              <h1>Screen Cart</h1>
+            </div>
+          </div>
+          <div className="col-md-12 col-lg-6">
+            <div className="mid d-flex align-items-center">
+              <select name="category" id="category" className="me-2">
+                <option value="category1">Category 1</option>
+                <option value="category2">Category 2</option>
+                <option value="category3">Category 3</option>
+              </select>
+              <input type="text" className="form-control me-2" placeholder="Search products..." />
+              <button className="btn btn-primary">Search</button>
+            </div>
+          </div>
+          <div className='col-md-12 col-lg-3'>
+            <div className='end text-center'>
+              <div className='buy-icons'>
+                <i class="fa-regular fa-heart"></i>
+                <h6>Your Wishlist</h6>
+              </div>
+              <div className='buy-icons'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <h6>Your Cart</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- Project/src/Pages/Website/Website.jsx
+++ Project/src/Pages/Website/Website.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import ContactBar from './Contact-bar/ContactBar';
+import Header from './Header/Header';
 
 
 const Website = () => {
   return (
     <div>
       <ContactBar />
+      <Header />
       <Outlet />
     </div>
   )
