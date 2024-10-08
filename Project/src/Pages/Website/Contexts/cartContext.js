9b554bd4eb1ba6e70019063434d1c666027abee5
Marawan Saqr <loew.krs@yahoo.com>
2024-10-08 18:37:59 +0300

    Added Message Show When Click On Product

--- Project/src/Pages/Website/Contexts/cartContext.js
+++ Project/src/Pages/Website/Contexts/cartContext.js
@@ -1,26 +1,48 @@
 import React, { createContext, useState } from 'react';
+import { Snackbar } from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
 
 
 export const CartContext = createContext();
-const CartProvider = ({children}) => {
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
 
   // Add To Cart Function
-  const [cart, setCart] = useState([]);
   const addToCart = (product) => {
     const isExist = cart.find(res => res.id === product.id);
     if (!isExist) {
       const newCart = [...cart, product];
       setCart(newCart);
+      setMessage(`${product.name} added to your cart!`);
+      setOpen(true);
     }
-  }
+  };
 
+  // Close Snackbar
+  const handleClose = (reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
   return (
-    <CartContext.Provider value={{cart, addToCart}}>
+    <CartContext.Provider value={{ cart, addToCart }}>
       {children}
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <MuiAlert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          {message}
+        </MuiAlert>
+      </Snackbar>
     </CartContext.Provider>
-  )
-}
-
+  );
+};
 
 export default CartProvider;
