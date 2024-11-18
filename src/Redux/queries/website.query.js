5a11d6753367dcfc724036ea8dd9742e01641648
Marawan Saqr <82732583+Marawan-Saqr@users.noreply.github.com>
2024-11-18 11:58:57 +0200

    Update website.query.js

--- src/Redux/queries/website.query.js
+++ src/Redux/queries/website.query.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const websiteQuery = createApi({
   reducerPath: 'websiteQuery',
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://loud-polished-grouse.glitch.me" }), // Replace localhost with Glitch URL
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => '/products',
@@ -17,7 +17,6 @@ export const websiteQuery = createApi({
         body: userData,
       }),
     }),
-    // Add the loginUser mutation
     loginUser: builder.mutation({
       query: (credentials) => ({
         url: '/websiteUsers',
